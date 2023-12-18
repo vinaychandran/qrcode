c1e02ddddefbf38606e37075c069bd43b977df9b
Vinay <Vinay@MHM-HO-1373.local>
2023-12-18 15:11:49 +0900

    styled for mobile

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import styles from './App.module.css';
+
 import EmailQRCodeGenerator from './qr-generator/EmailQRCodeGenerator'
 import AddressQRCodeGenerator from './qr-generator/AddressQRCodeGenerator';
 
 function App() {
   return (
-    <div style={{display:'flex', justifyContent:'space-around'}}> 
-    <EmailQRCodeGenerator />
-    <AddressQRCodeGenerator />
+    <div className={styles.app}> 
+      <EmailQRCodeGenerator />
+      <AddressQRCodeGenerator />
     </div>
   );
 }
 
 export default App;
+
+
